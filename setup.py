fa3d33f9d46544bd293148640d0f987be9a0e3f8
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2020-08-02 14:37:13 -0300

    Bump version: 1.0.0 → 1.1.0

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/esequiel378/proxy_randomizer',
-    version='1.0.0',
+    version='1.1.0',
     zip_safe=False,
 )
