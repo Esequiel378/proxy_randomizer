ccd8b35c994e2bcde897fb9974d6bdb832237422
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2020-08-02 12:48:26 -0300

    Bump version: 0.1.0 → 1.0.0

--- proxy_randomizer/__init__.py
+++ proxy_randomizer/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Esequiel Albornoz"""
 __email__ = 'esequielalbornoz7@gmail.com'
-__version__ = '0.1.0'
+__version__ = '1.0.0'
--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/esequiel378/proxy_randomizer',
-    version='0.1.0',
+    version='1.0.0',
     zip_safe=False,
 )
