3248bc2c246b2d964aecaac50df849eb6d504ff3
Esequiel Albornoz <esequielalbornoz7@gmail.com>
2023-01-03 12:34:34 -0300

    Bump version: 1.3.4 → 1.3.5

--- proxy_randomizer/__init__.py
+++ proxy_randomizer/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Esequiel Albornoz"""
 __email__ = "esequielalbornoz7@gmail.com"
-__version__ = "__version__ = '1.3.4'"
+__version__ = "__version__ = '1.3.5'"
 
 from .providers import RegisteredProviders
 from .proxy import Proxy
